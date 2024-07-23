5065580b20a1f7b6b9e544ae75f52c35a105a244
golphat1 <golphat@yahoo.com>
2024-07-23 14:53:11 +0300

    Created README.md with initial project details

---
+++ client/src/Post.js
@@ -0,0 +1,18 @@
+export default function Post() {
+    return (
+        <div className="post">
+        <div className="img">
+        <img src="https://plus.unsplash.com/premium_photo-1683120966127-14162cdd0935?q=80&w=1326&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="AI technology" />
+        </div>
+        <div className="text">
+          <h2>AI technology</h2>
+          <p className="info">
+            <a className="author">Golphat Wanyonyi</a>
+            <time>2024-07-20 15:51</time>
+          </p>
+          <p className="suammary">AI technology encompasses various techniques and tools designed to simulate human intelligence in machines.</p>
+        </div>
+      </div>
+      
+    );
+}
