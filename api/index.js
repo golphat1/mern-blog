f9043ad2dbabc90e7dc5d177a92010536a3b972d
golphat1 <golphat@yahoo.com>
2024-07-23 20:47:41 +0300

    Installed Express and Mongoose

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+const User = require('./models/User');
+const Post = require('./models/Post');
+const bcrypt = require('bcryptjs');
+const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
